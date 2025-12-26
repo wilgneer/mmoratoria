238fe343c61fe5d23fca758e9273a958ddc984d7
Wilgner Bastos <wilgneerbastos@gmail.com>
2025-12-25 22:09:09 -0300

    Melhoria final para desempenho
    Melhoria final de desempenho do site

--- script.js
+++ script.js
@@ -286,4 +286,10 @@ function setupLiteYouTube() {
 
 setupLiteYouTube();
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
+
 }
